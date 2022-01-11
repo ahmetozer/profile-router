ae0a2f91a523612b0e9245917006134f08c9bc30
ahmetozer <git@ahmetozer.org>
2022-01-11 21:21:43 +0300

    fix: profile select problem

--- linkRouter/executer.go
+++ linkRouter/executer.go
@@ -1,8 +1,10 @@
 package linkrouter
 
 import (
+	"bytes"
 	"log"
 	"os"
+	"os/exec"
 
 	"github.com/ahmetozer/profile-router/config"
 )
@@ -10,37 +12,14 @@ import (
 // Executer
 // Execute Google Chrome
 func Executer(Config *config.Settings, args *[]string) {
-
-	//var cred = &syscall.Credential{UID, GUID, []uint32{}}
-	//var sysproc = &syscall.SysProcAttr{Credential: cred, Noctty: true}
-	var attr = os.ProcAttr{
-		Dir: ".",
-		Env: os.Environ(),
-		Files: []*os.File{
-			os.Stdin,
-			nil,
-			nil,
-		},
-	}
-	process, err := os.StartProcess(Config.ChromePath, *args, &attr)
-	if err == nil {
-
-		// It is not clear from docs, but Realease actually detaches the process
-		err = process.Release()
-		if err != nil {
-			log.Fatalf(err.Error())
-		}
-
-	} else {
+	cmd := exec.Command(Config.ChromePath, *args...)
+	err := cmd.Start()
+	if err != nil {
 		log.Fatalf(err.Error())
 	}
-	// cmd := exec.Command(Config.ChromePath, *args...)
-	// err := cmd.Run()
-
-	// var stdout, stderr bytes.Buffer
-	// cmd.Stdout = &stdout
-	// cmd.Stderr = &stderr
-	// return stdout.String(), stderr.String(), cmd.ProcessState.ExitCode()
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
 
 	// Then app exists
 	os.Exit(0)
